b563e082dfc2a9386ce29895224b1af1c3dcc611
Abel Sibi <abelsibi05@gmail.com>
2024-10-06 11:56:19 +0530

    new files added

--- hello_django/hello_app/views.py
+++ hello_django/hello_app/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def start(request):
-    variable1={'title':'ARM','year':2024,'summary':'action movie','success': False}
-    return render(request,'hello.html',variable1)
+    movie_list={'movies':[{'title':'ARM','year':2024,'summary':'action movie','success': True},
+                {'title':'Vazha','year':2024,'summary':'love and comedy','success': True},
+                {'title':'Janeman','year':2019,'summary':'comedy drama','success': True},
+                {'title':'yavanika','year':1984,'summary':'crime thriller','success': False},
+                {'title':'CID moosa','year':1984,'summary':'','success': False}]}
+    return render(request,'hello.html',movie_list)
