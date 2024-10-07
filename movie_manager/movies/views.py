8597485ec8bac66bb0f2cebab3526181f0abc021
abelsibi <abelx.sibi@intel.com>
2024-10-07 18:56:01 +0530

    new

--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 def create(request):
     return render(request,'create.html')
 def list(request):
-    return render(request,'list.html')
+    movie_list={'movies':[{'title':'ARM','year':2024,'summary':'action movie','success': True},
+                {'title':'Vazha','year':2024,'summary':'love and comedy','success': True},
+                {'title':'Janeman','year':2019,'summary':'comedy drama','success': True},
+                {'title':'yavanika','year':1984,'summary':'crime thriller','success': False},
+                {'title':'CID moosa','year':1984,'summary':'','success': False}]}
+    return render(request,'list.html',movie_list)
+    
 def edit(request):
     return render(request,'edit.html')
